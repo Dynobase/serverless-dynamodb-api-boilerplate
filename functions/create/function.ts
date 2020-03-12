096d39691d9236619a68e6e27a0a278ac0e8dbe9
Rafal Wilinski <raf.wilinski@gmail.com>
2020-03-12 08:56:13 +0100

    Initial commit

---
+++ functions/create/function.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+import { Model } from 'dynamodb-toolbox';
+
+export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
